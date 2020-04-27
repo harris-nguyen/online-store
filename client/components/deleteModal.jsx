43dabd97d1bef985c5e5eccf13133084af00a936
harris-nguyen <harrisq.nguyen@gmail.com>
2020-04-27 21:38:53 +0000

    modal for are u sure u want to delete works

--- client/components/deleteModal.jsx
+++ client/components/deleteModal.jsx
@@ -16,8 +16,8 @@ export default class DeleteModal extends React.Component {
               Item Added
             </Modal.Title>
           </Modal.Header>
-          <Modal.Body>Are you sure you want to delete?</Modal.Body>
-          <div className="container">
+          <div className="container text-center">
+            <Modal.Body>Are you sure you want to delete this item?</Modal.Body>
             <div>
               <img
                 src={this.props.image}
@@ -31,17 +31,17 @@ export default class DeleteModal extends React.Component {
           </div>
           <Modal.Footer>
             <div>
-              <button className="btn btn-primary" onClick={this.props.onHide}>
+              <button className="btn btn-warning" onClick={this.props.onHide}>
                 No
               </button>{' '}
-              <div onClick={this.props.onHide}>
+              <span onClick={this.props.onHide}>
                 <button
                   className="btn btn-danger"
                   onClick={this.props.onclickDel}
                 >
                   Yes
                 </button>
-              </div>
+              </span>
             </div>
             <div></div>
           </Modal.Footer>
